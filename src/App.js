483e6f07b69af80903938c882f48f453f1a77ef3
Victoria <victoria.ancalaf@gmail.com>
2021-02-14 00:03:08 -0300

    song library component added

--- src/App.js
+++ src/App.js
@@ -3,15 +3,22 @@ import Player from "./components/Player";
 import Song from "./components/Song";
 import "./styles/app.scss";
 import data from "./data";
+import Library from "./components/Library";
 
 function App() {
   const [songs, setSongs] = useState(data());
   const [currentSong, setCurrentSong] = useState(songs[6]);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   return (
     <div className="App">
       <Song currentSong={currentSong} />
-      <Player />
+      <Player
+        isPlaying={isPlaying}
+        setIsPlaying={setIsPlaying}
+        currentSong={currentSong}
+      />
+      <Library songs={songs} currentSong={currentSong} />
     </div>
   );
 }
